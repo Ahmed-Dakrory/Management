5a899323411aac48952ffe556a09f391cacf1f43
Ahmed Dakrory <ahmed.aldakrory@tayseerme.com>
2022-10-31 05:03:04 +0200

    add all

--- Management/settings.py
+++ Management/settings.py
@@ -108,7 +108,7 @@ else:
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'management',                      
         'USER': 'root',                      
-        'PASSWORD': '',          
+        'PASSWORD': 'password',          
         'HOST': '127.0.0.1',                 
         'PORT': '3306', 
         'TIME_ZONE': 'Africa/Cairo',                     
